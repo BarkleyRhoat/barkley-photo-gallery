be5ede37c2971180e26a9030f9bffc975649f78a
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-06-29 14:54:48 -0400

    implement feature 3
    delete photo with photocard componenet, added css for photocard

--- my-app/src/components/PhotoCard.js
+++ my-app/src/components/PhotoCard.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function PhotoCard({ photo, onDelete }) {
+	return (
+		<div className="photo-card">
+			<img src={photo.url} alt="gallery" />
+			<button onClick={() => onDelete(photo.id)}>Delete</button>
+		</div>
+	);
+}
+
+export default PhotoCard;
