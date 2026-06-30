39322464b0c45cdf03388a20a30f82dcc7566e1a
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-06-30 09:19:00 -0400

    bones for aboutpage
    added <Header/> in errorpage instead of <NavBar/>

--- my-app/src/pages/AboutPage.js
+++ my-app/src/pages/AboutPage.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
 
+function AboutPage() {
+  return (
+		<div>
+			<Header />
+			<h1>About</h1>
+		</div>
+	);
+}
+
+export default AboutPage
